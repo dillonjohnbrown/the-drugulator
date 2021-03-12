08cd309c188bf91ca910bd50182c7b8f91fae190
dillonjohnbrown <49077521+dillonjohnbrown@users.noreply.github.com>
2021-03-12 14:34:13 -0800

    Replace most jQuery with JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-var data, drugValue1, drugValue2;
+var data, drugValue1, drugValue2, drugSelect1, drugSelect2;
+
+var displayResult = document.getElementById('displayResult');
+var displayNote   = document.getElementById('displayNote');
 
 var getJSON = function(url, callback) {
 	var xhr = new XMLHttpRequest();
@@ -16,7 +19,7 @@ var getJSON = function(url, callback) {
 };
 getJSON('js/combo_beta.json', function(err, data) {
 	if (err !== null) {
-		alert('Something went wrong: ' + err);
+		alert('Something went wrong, please reload this page. Error: ' + err);
 	} else {
 		var items = [];
 		$.each( data, function( key ) {
@@ -24,18 +27,26 @@ getJSON('js/combo_beta.json', function(err, data) {
 		});
 	
 		// Create select (dropdown #2) element
+		// TODO replace jQuery with JavaScript
 		$( "<select/>", {
 			"id": "drugSelect2",
 			"onchange": "getDrugValue2()",
 			html: items.join( "" )
 		}).insertAfter( "#drugLabel2" );
+		
+		// Update variable
+		drugSelect2 = document.getElementById("drugSelect2");
 	
 		// Create select (dropdown #1) element
+		// TODO replace jQuery with JavaScript
 		$( "<select/>", {
 			"id": "drugSelect1",
 			"onchange": "getDrugValue1()",
 			html: items.join( "" )
 		}).insertAfter( "#drugLabel1" );
+		
+		// Update variable
+		drugSelect1 = document.getElementById("drugSelect1");
 	
 		// Add placeholders to select elements
 		$('#drugSelect1')
@@ -66,8 +77,7 @@ readTextFile("js/combo_beta.json", function(text) {
 
 // Get the selected drug from dropdown #1
 function getDrugValue1() {
-	var x = document.getElementById("drugSelect1");
-	drugValue1 = x.options[x.selectedIndex].value;
+	drugValue1 = drugSelect1.options[drugSelect1.selectedIndex].value;
 	
 	// Show result
 	compareDrugs();
@@ -75,8 +85,7 @@ function getDrugValue1() {
 
 // Get the selected drug from dropdown #2
 function getDrugValue2() {
-	var x = document.getElementById("drugSelect2");
-	drugValue2 = x.options[x.selectedIndex].value;
+	drugValue2 = drugSelect2.options[drugSelect2.selectedIndex].value;
 	
 	// Show result
 	compareDrugs();
@@ -88,22 +97,22 @@ function compareDrugs() {
 	
 	if (drugValue1 == drugValue2) {
 		// Drugs are the same and not in JSON file
-		$("#displayResult").text("Good lookin' son!");
-		$("#displayNote").text("If you take " + drugValue1 + " and more " + drugValue2 + ", well you're just taking one drug now aren't you?");
+		displayResult.textContent = "Good lookin' son!";
+		displayNote.textContent = "If you take " + drugValue1 + " and more " + drugValue2 + ", well you're just taking one drug now aren't you?";
 	} else if (typeof drugValue2 == 'undefined' || typeof drugValue1 == 'undefined') {
 		// One select elements does not have a value
-		console.log("One of the dropdowns hasn't been clicked");
+		// console.log("One of the dropdowns hasn't been clicked");
 	} else {
 		// Drugs are different
 		var drugCombinationResult = data[drugValue1][drugValue2];
-	
-		$("#displayResult").text( drugCombinationResult.status );
+		
+		displayResult.textContent = drugCombinationResult.status;
 		
 		// If specific combination has a note, print it, otherwise clear it
-		if ( typeof drugCombinationResult.note != "undefined" ) {
-			$("#displayNote").html( drugCombinationResult.note );
+		if (typeof drugCombinationResult.note != "undefined") {
+			displayNote.innerText = drugCombinationResult.note;
 		} else {
-			$("#displayNote").text("");
+			displayNote.textContent = "";
 		}
 	}
 }

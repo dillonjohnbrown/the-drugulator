5c7ffb0539966d22df53c71f384fa0690b0308fe
dillonjohnbrown <49077521+dillonjohnbrown@users.noreply.github.com>
2023-04-17 09:38:24 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -17,15 +17,15 @@ var explanation__unknown          = document.getElementById('displayExplanation_
 	Get TripSit's big JSON file
 */
 function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = function() {
+		if (rawFile.readyState === 4 && rawFile.status == "200") {
+			callback(rawFile.responseText);
+		}
+	}
+	rawFile.send(null);
 }
 readTextFile("js/combo_beta.json", function(text) {
 	// Make sense of the JSON file
